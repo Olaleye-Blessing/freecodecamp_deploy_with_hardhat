d29bfc354ff1eaebb8b925a2c465d9e1137624bf
Olaleye Blessing <olayinkablexxy@gmail.com>
2022-06-21 19:43:17 +0100

    add gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config({ path: "./.env" });
 require("@nomiclabs/hardhat-etherscan");
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -12,6 +13,7 @@ require("./tasks/block-number");
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const EHERSCAN_API_KEY = process.env.EHERSCAN_API_KEY;
+const COIN_MARKET_CAP_API_KEY = process.env.COIN_MARKET_CAP_API_KEY;
 
 module.exports = {
   // hardhat has its default network, rpc url and private key
@@ -33,5 +35,12 @@ module.exports = {
   solidity: "0.8.8",
   etherscan: {
     apiKey: EHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COIN_MARKET_CAP_API_KEY,
   },
 };
